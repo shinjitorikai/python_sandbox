474c7675bd2ab5bdb80c923e48bffce183b85ce1
shinjitorikai <55309564+shinjitorikai@users.noreply.github.com>
2020-05-24 15:58:18 +0900

    Add basic/basic.py, flow.py

---
+++ basic/basic.py
@@ -0,0 +1,35 @@
+
+a = 1
+
+# data type : type()関数でデータの型を調べることができる
+print('type = ',type(a))
+print('type = ',type(3.14))
+print('type = ',type("abc"))
+
+# list
+listdata = [1,2,3,4,5]
+print('listdata = ', listdata)
+print(' - length = ', len(listdata))
+print(' - 要素アクセス a[0] = ', listdata[0])
+print(' - スライシング a[0:2] = ', listdata[0:2]) # 0番目から2番目まで(2番目は含まない)
+print(' - スライシング a[2:] = ', listdata[2:]) # 2番目以降
+print(' - スライシング a[:2] = ', listdata[:2]) # 2番目まで(2番目は含まない)
+print(' - スライシング a[:-1] = ', listdata[:-1]) # 最後から1つ前まで(最後から1つ前は含まない)
+print(' - スライシング a[:-2] = ', listdata[:-2]) # 最後から2つ前まで(最後から2つ前は含まない)
+
+# dictionary
+dicdata = {'key1':12,'key2':34,'key4':78}
+print('dicdata = ', dicdata)
+print(' - 要素アクセス key1 : ', dicdata['key1'])
+dicdata['key3'] = 56
+print(' - 要素追加 : ', dicdata)
+
+# logical calc
+bData1 = True
+bData2 = False
+print('論理演算')
+print(' - not bData1 : ', not bData1)
+print(' - and bData1 : ', bData1 and bData2)
+print(' - or  bData1 : ', bData1 or bData2)
+
+
---
+++ basic/flow.py
@@ -0,0 +1,15 @@
+# if
+x = 12
+if x > 10:
+    print('x is larger than 10')
+elif x > 5:
+    print('x is larger than 5')
+else:
+    print('x is less than 5')
+
+# for
+a = [1,2,3]
+print('for')
+for tmp in a:
+    print(' - tmp : ',tmp)
+
