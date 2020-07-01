60919693f388817cadea721c647ac007e2592a58
shinjitorikai <55309564+shinjitorikai@users.noreply.github.com>
2020-07-01 20:56:55 +0900

    Modify basic/basic.py (reversed/enumerate/zip/iterator)

--- basic/basic.py
+++ basic/basic.py
@@ -36,3 +36,45 @@ print('論理演算')
 print(' - not bData1 : ', not bData1)
 print(' - and bData1 : ', bData1 and bData2)
 print(' - or  bData1 : ', bData1 or bData2)
+
+# reversed/enumerate
+r = range(3)
+print('range(3) = ',r) # range(0, 3)
+r_l = list(r)
+print('list(range(3)) = ',r_l) # [0, 1, 2]
+r_rev = reversed(r)
+print('reversed(range(3)) = ',r_rev) # <range_iterator object>(逆転)
+print('list(reversed(range(3))) = ',list(r_rev)) # [2, 1, 0]
+
+# zip
+list1 = [1,2,3,4]
+list2 = ['A','B','C']
+list_zip = zip(list1,list2)
+print('zip(list1,list2) = ',list_zip) # <zip object>
+print('zip(list1,list2) = ',list(list_zip)) # [(1, 'A'), (2, 'B'), (3, 'C')] (多い方は無視)
+
+# enumerate
+r_e = enumerate(list2)
+print('enumerate(list2) = ',r_e)
+print('list(enumerate(list2)) = ',list(r_e))
+
+# リスト内包表記(list)
+tmp1 = [i*2 for i in range(3)]
+print('[i*2 for i in range(3)] = ',tmp1)
+tmp2 = [i for i in range(10) if i%2==0]
+print('[i for i in range(10) if i%2==0] = ',tmp2)
+
+# 集合内包表記(set)
+tmp3 = {i*2 for i in range(3)}
+print('{i*2 for i in range(3)} = ',tmp3)
+
+# 辞書内包表記(dictionaryの生成)
+tmp4 = ['A','B','C'] # list
+tmp5 = {v:i for i,v in enumerate(tmp4)}
+print('{v:i for i,v in enumerate(tmp4)} = ',tmp5) # {'A': 0, 'B': 1, 'C': 2}
+
+# イテレーター
+it = iter(list1)
+print('iter(list1) = ',it) # <list_iterator object>
+print('next(it) = ',next(it)) # 1
+print('next(it) = ',next(it)) # 2
