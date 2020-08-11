d64eeba8101e100552554718e8c0561094c12e16
shinjitorikai <55309564+shinjitorikai@users.noreply.github.com>
2020-08-11 15:03:27 +0900

    Add math/basic_stat.py

---
+++ math/basic_stat.py
@@ -0,0 +1,12 @@
+# 統計関連基礎
+import math
+import scipy.special
+
+# 階乗
+print('3! = ',math.factorial(3))
+
+# 順列(nPr)
+print('6P4 = ',scipy.special.perm(6,4))
+
+# 組み合わせ(nCr)
+print('6C4 = ',scipy.special.comb(6,4))
