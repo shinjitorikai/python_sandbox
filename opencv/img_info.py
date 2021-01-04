c92fe0ec746fbb0297a855abc2cbae9097ad3f68
shinjitorikai <55309564+shinjitorikai@users.noreply.github.com>
2021-01-04 11:27:55 +0900

    opencv/img_info.py Add imread error notify

--- opencv/img_info.py
+++ opencv/img_info.py
@@ -3,12 +3,15 @@ import cv2
 
 img = cv2.imread('./opencv/test.jpg')
 
-print('img type : ',type(img))
-print('img.shape : ',img.shape)
-print('img.dtype : ',img.dtype)
-print('img.size : ',img.size)
+if img is None:
+    print('img read error')
+else:
+    print('img type : ',type(img))
+    print('img.shape : ',img.shape)
+    print('img.dtype : ',img.dtype)
+    print('img.size : ',img.size)
 
-#cv2.imshow('test',img)
+    #cv2.imshow('test',img)
 
-#cv2.waitKey()
-#cv2.destroyAllWindows()
+    #cv2.waitKey()
+    #cv2.destroyAllWindows()
