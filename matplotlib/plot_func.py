8254f623956198f581779ebe183904979a1aa680
shinjitorikai <55309564+shinjitorikai@users.noreply.github.com>
2021-08-13 12:34:24 +0900

    Modify matplotlib/plot_func.py for drawing graph

--- matplotlib/plot_func.py
+++ matplotlib/plot_func.py
@@ -1,16 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def sigmoid(x):
+# 設定値
+X_MIN = -5.0
+X_MAX = 5.0
+Y_MIN = -0.1
+Y_MAX = 1.1
+X_STEP = 0.1
+PLOT_TITLE = 'sigmoid function'
+def func_plot(x):
   return 1 / (1+np.exp(-x))
 
-x = np.arange(-5.0, 5.0, 0.1)
-y = sigmoid(x)
+# メイン処理
+def main():
+  x = np.arange(X_MIN, X_MAX, X_STEP)
+  y = func_plot(x)
 
-plt.plot(x, y)
-plt.ylim(-0.1, 1.1) # y軸の範囲設定
-plt.xlabel('x')
-plt.ylabel('y')
-plt.grid(which='both')
-plt.title('sigmoid function')
-plt.show()
+  plt.plot(x, y)
+  plt.ylim(Y_MIN, Y_MAX) # y軸の範囲設定
+  plt.xlabel('x')
+  plt.ylabel('y')
+  plt.grid(which='both')
+  plt.title(PLOT_TITLE)
+  plt.show()
+
+if __name__ == '__main__':
+    main()
