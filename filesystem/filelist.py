a7b95c980175216c68adddebed1b9001edcf2087
shinjitorikai <55309564+shinjitorikai@users.noreply.github.com>
2021-12-23 19:20:42 +0900

    Modify filesystem/fielist.py - functionalize

--- filesystem/filelist.py
+++ filesystem/filelist.py
@@ -1,32 +1,50 @@
+import os
 from pathlib import Path
 from tqdm import tqdm
 
 # 設定値
-target_dir = r'F:\整理中'
-#search_cond = '*' # ターゲットディレクトリの、ファイルおよびフォルダを取得
-#search_cond = '*.sln' # 条件あり
-search_cond = '**/*' # サブディレクトリも含む
-output_filename = r'.\filesystem\output.csv'
+TARGET_DIR = r'/Users/torikai/Documents/sandbox/python_sandbox'
+SEARCH_PATTERN = '*'
+ISRECURSIVE = True
+OUTPUT_FILENAME = r'./filesystem/output.csv'
 
+def getfilelist(targetdir,pattern,isrecursive):
+    p = Path(targetdir)
 
-# ファイル/フォルダを取得
-print('ファイル/フォルダを取得...')
-pt = Path(target_dir)
-resultlist = list(pt.glob(search_cond))
+    if isrecursive == True:
+        search_pattern = '**/' + pattern
+    else:
+        search_pattern = pattern
+    
+    filelist = [str(filename) for filename in list(p.glob(search_pattern))]
 
-# 結果をファイルに出力
-print('結果をファイルに出力...')
-#with open(output_filename,mode='w',encoding='utf-8') as fw: # utf-8だとExcelで開いたときに文字化け
-with open(output_filename,mode='w') as fw:
+    return filelist
 
-    fw.write('path,dir,file,ext\n') # タイトル行
-    for line in tqdm(resultlist):
-        if line.is_file():
-            # ファイルのみ書き込み(ディレクトリは書き込まない)
-            strpath = str(line)
-            strdirname = str(line.parent)
-            strfilename = line.name
-            strext = line.suffix
-            fw.write(strpath+','+strdirname+','+strfilename+','+strext+'\n')
+def main():
+    # ファイル/フォルダを取得
+    print('ファイル/フォルダを取得...')
+    filelist = getfilelist(TARGET_DIR, SEARCH_PATTERN, ISRECURSIVE)
 
-print('処理終了')
+    # 結果をファイルに出力
+    print('結果をファイルに出力...')
+    file_num = 0
+    #with open(OUTPUT_FILENAME,mode='w',encoding='utf-8') as fw: # utf-8だとExcelで開いたときに文字化け
+    with open(OUTPUT_FILENAME,mode='w') as fw:
+
+        fw.write('path,dir,file,ext,size_byte\n') # タイトル行
+        for line in tqdm(filelist):
+            if os.path.isfile(line):
+                # ファイルのみ書き込み(ディレクトリは書き込まない)
+                strpath = line
+                strdirname = os.path.dirname(line)
+                strfilename = os.path.basename(line)
+                _,strext = os.path.splitext(line)
+                strfilesize_byte = str(os.path.getsize(line))
+                fw.write(strpath+','+strdirname+','+strfilename+','+strext+','+strfilesize_byte+'\n')
+                file_num += 1
+
+    print('file_num : ',file_num)
+    print('処理終了')
+
+if __name__ == '__main__':
+    main()
